cedec8108f7c5a1d0bc07e5a139bb563a0840032
Alexandr Zanko <alexandrzanko@Alexandrs-MacBook-Air.local>
2017-02-22 15:58:21 +0300

    загрузка проекта

---
+++ app/src/main/java/com/example/alexandrzanko/mobile_6vkusov/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alexandrzanko.mobile_6vkusov;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
